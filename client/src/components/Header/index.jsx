85ef8b1a44059abcb309e4bb9786c791e6e0b7c0
Viktor Filippov <jackal3166@gmail.com>
2023-07-23 15:45:01 +0300

    Create Layout

---
+++ client/src/components/Header/index.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+import styles from './Header.module.scss';
+import Container from '@mui/material/Container';
+
+export const Header = () => {
+    const isAuth = false;
+
+    const onClickLogout = () => {};
+
+    return (
+        <div className={styles.root}>
+            <Container maxWidth='lg'>
+                <div className={styles.inner}>
+                    <a className={styles.logo} href='/'>
+                        <div>VIKTOR FILIPPOV</div>
+                    </a>
+                    <div className={styles.buttons}>
+                        {isAuth ? (
+                            <>
+                                <a href='/posts/create'>
+                                    <Button variant='contained'>
+                                        Write an article
+                                    </Button>
+                                </a>
+                                <Button
+                                    onClick={onClickLogout}
+                                    variant='contained'
+                                    color='error'>
+                                    Get out
+                                </Button>
+                            </>
+                        ) : (
+                            <>
+                                <a href='/login'>
+                                    <Button variant='outlined'>Log in</Button>
+                                </a>
+                                <a href='/register'>
+                                    <Button variant='contained'>
+                                        Create an account
+                                    </Button>
+                                </a>
+                            </>
+                        )}
+                    </div>
+                </div>
+            </Container>
+        </div>
+    );
+};
